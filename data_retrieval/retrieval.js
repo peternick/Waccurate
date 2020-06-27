6063be1154ea492b7819a67f82f3207a0d0a92d5
PeterA <50685884+peternick@users.noreply.github.com>
2020-06-26 21:49:48 -0400

    Finished retrieval script to retrieve api data
    The script used to continuously retrieve and store Accuweather api data for the Waccurate app is finished. It retrieves statistics from the Accuweather api and stores it in a file. All that needs to be done is to continuously scrape data from Accuweather so that the Waccurate app has enough data to make conclusions about how accurate Accuweather really is.

--- data_retrieval/retrieval.js
+++ data_retrieval/retrieval.js
@@ -52,59 +52,104 @@ function initialize_city_requests(){
 
 
 
-// intialize_location_id()
-// function intialize_location_id(){
-//     for(state of Object.keys(cities_forecast_dict)){
-//         for(city of Object.keys(cities_forecast_dict[state])){
-//             var country_id = ''
-//             var state_local_name = ''
-//             fetch('http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city)
-//             .then(response => response.json())
-//             .then(loc => { 
-//                 country_id = loc[0]['Country']['ID']
-//                 state_local_name = loc[0]['AdministrativeArea']['LocalizedName']
-//                 if (country_id == 'US' && state_local_name == state){
-//                     cities_forecast_dict[state][city] = loc[0]['Key']
-//                 }
-//                 else{
-//                     console.log('error! ' + loc[0]['Country']['ID'] + ' or ' + loc[0]['AdministrativeArea']['LocalizedName']+ ' is not in the dictionary')
-//                 }
-//             })
-//             .then(res =>{
-//                 intialize_forecast_data(state, city);
-//             })
-//         }
-//     }
-// }
+intialize_location_id()
+function intialize_location_id(){
+    for(state of Object.keys(cities_forecast_dict)){
+        for(city of Object.keys(cities_forecast_dict[state])){
+            var country_id = ''
+            var state_local_name = ''
+            fetch('http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city)
+            .then(response => response.json())
+            .then(loc => { 
+                country_id = loc[0]['Country']['ID']
+                state_local_name = loc[0]['AdministrativeArea']['LocalizedName']
+                if (country_id == 'US' && state_local_name == state){
+                    cities_forecast_dict[state][city] = loc[0]['Key']
+                }
+                else{
+                    console.log('error! ' + loc[0]['Country']['ID'] + ' or ' + loc[0]['AdministrativeArea']['LocalizedName']+ ' is not in the dictionary')
+                }
+            })
+            .then(res =>{
+                intialize_forecast_data(state, city);
+            })
+        }
+    }
+}
 
-// function intialize_forecast_data(state, city){
-//     fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + cities_forecast_dict[state][city] + '?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&details=true')
-//     .then(res => res.json())
-//     .then(forecast =>{
-//         var loc_key = cities_forecast_dict[state][city]
-//         cities_forecast_dict[state][loc_key] = forecast
-//     })
-//     .then(good =>{
-//         fs.writeFile(
-//             path.join(__dirname, './', 'hello.txt'),
-//             JSON.stringify(cities_forecast_dict),
-//             err => {
-//               if (err) throw err;
-//               console.log('File written');
-//             }
-//         );
-//     })
-//     .then(print =>{
-//         after();
-//     })
-// }
+function intialize_forecast_data(state, city){
+    fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + cities_forecast_dict[state][city] + '?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&details=true')
+    .then(res => res.json())
+    .then(forecast_data =>{
+       var loc_key = cities_forecast_dict[state][city]
+        cities_forecast_dict[state][loc_key] = initialize_weather_map(forecast_data)
+    })
+    .then(res =>{
+        fs.readFile(path.join(__dirname, "./new_format.txt"), "utf8", (err, file_text) => {
+            if (err) throw err;
+        
+            if(file_text.length == 0){
+                var curr_date_id = new Date()
+                curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
+                var wacc_dict = {};
+                wacc_dict[curr_date_id] = cities_forecast_dict; 
+                fs.appendFile(
+                    path.join(__dirname, './', 'new_format.txt'),
+                    JSON.stringify(wacc_dict),
+                    err => {
+                    if (err) throw err;
+                    console.log('File written');
+                    }
+                );
+            }
+            else{
+                file_text = file_text.substring(0, file_text.length - 1)
+                file_text = file_text + ','
+                var curr_date_id = new Date()
+                curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
+                var wacc_dict = {};
+                wacc_dict[curr_date_id] = cities_forecast_dict; 
+                var added_dictionary = (JSON.stringify(wacc_dict))
+                added_dictionary = added_dictionary.substring(1, added_dictionary.length);
+                file_text = file_text + added_dictionary;
+                fs.writeFile(
+                    path.join(__dirname, './', 'new_format.txt'),
+                    file_text,
+                    err => {
+                    if (err) throw err;
+                    console.log('File appended');
+                    }
+                );
+                
+            }
+        });
+    })
+    .then(print =>{
+        //after();
+    })
+}
 
-// function after(){
-//     console.log(cities_forecast_dict)
-// }
+function initialize_weather_map(forecast_data){
+    var wacc_forecast = {}
+    for(data_hr of forecast_data){
+        var date_code = new Date(data_hr['DateTime'])
+        date_code = '' + date_code.getHours();
+        wacc_forecast[date_code] = {};
+        wacc_forecast[date_code]['Temperature'] = data_hr['Temperature']['Value']
+        wacc_forecast[date_code]['Weather'] = data_hr['IconPhrase']
+        wacc_forecast[date_code]['RealFeelTemperature'] = data_hr['RealFeelTemperature']['Value']
+        wacc_forecast[date_code]['PrecipitationProbability'] = data_hr['PrecipitationProbability']
+        wacc_forecast[date_code]['RainProbability'] = data_hr['RainProbability']
+        wacc_forecast[date_code]['SnowProbability'] = data_hr['SnowProbability']
+        wacc_forecast[date_code]['RainVal'] = data_hr['Rain']
+        wacc_forecast[date_code]['SnowVal'] = data_hr['Snow']
+    }
+    return wacc_forecast
+}
 
-
-
+function after(){
+    console.log(cities_forecast_dict)
+}
 
 
 /*fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/329551?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB')
@@ -129,4 +174,7 @@ function initialize_city_requests(){
         );
         console.log(data)
     }).catch(err => console.log('err'))*/
+
+
+  
 
