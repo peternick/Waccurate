c790fcb1abdecf7a2250f749244096e36f8a8dfb
PeterA <50685884+peternick@users.noreply.github.com>
2020-07-02 23:30:11 -0400

    created script to retrieve api data for all states
    Data can now be retrieved for each state with retrieval.js where 12 hours of forecast can be stored.

--- data_retrieval/retrieval.js
+++ data_retrieval/retrieval.js
@@ -3,130 +3,51 @@ const path = require('path');
 const fetch = require('node-fetch')
 
 
-var cities_forecast_dict = {'Alabama': {'Montgomery':''}/*, 'Alaska': {'Juneau': ''}, 'Arizona': {'Phoenix': ''}, 'Arkansas': {'Little Rock': ''}, 
-'Connecticut': {'Hartford': ''}, 'Delaware': {'Dover': ''}, 'Florida': {'Tallahassee': ''}, 'Georgia': {'Atlanta': ''}, 
-'Indiana': {'Indianapolis': ''}, 'Kentucky': {'Frankfort': ''}, 'Maine': {'Augusta': ''}, 'Maryland': {'Annapolis': ''}, 'Massachusetts': {'Boston': ''}, 
-'Mississippi': {'Jackson': ''}, 'New Hampshire': {'Concord': ''}, 'New Jersey': {'Trenton': ''}, 'New York': {'Albany': ''}, 'North Carolina': {'Raleigh': ''}, 
-'Ohio': {'Columbus': ''}, 'Pennsylvania': {'Harrisburg': ''}, 'Rhode Island': {'Providence': ''}, 'South Carolina': {'Pierre': ''}, 'Tennessee': {'Nashville': ''}, 
-'Vermont': {'Montpelier': ''}, 'Virginia': {'Richmond': ''}, 'West Virginia': {'Charleston': ''}*/};
-
-/*initialize_city_requests()
-function initialize_city_requests(){
-    for(state of Object.keys(cities_forecast_dict)){
-        for(city of Object.keys(cities_forecast_dict[state])){
-            var country_id = ''
-            var state_local_name = ''
-            var location_data = ''
-            fetch('http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city)
-            .then(response => response.json())
-            .then(loc => { 
-                country_id = loc[0]['Country']['ID']
-                location_data = loc
-                state_local_name = loc[0]['AdministrativeArea']['LocalizedName']
-                if (country_id == 'US' && state_local_name == state){
-                    cities_forecast_dict[state][city] = loc[0]['Key']
-                    return fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + cities_forecast_dict[state][city] + '?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&details=true')
-                }
-                else{
-                    console.log('error! ' + loc[0]['Country']['ID'] + ' or ' + loc[0]['AdministrativeArea']['LocalizedName']+ ' is not in the dictionary')
-                }
-            }).then(res => res.json())
-            .then(forecast =>{
-                cities_forecast_dict[state][location_data[0]['Key']] = JSON.stringify(forecast)
-            })
-            .then(good =>{
-                fs.writeFile(
-                    path.join(__dirname, './', 'hello.txt'),
-                    JSON.stringify(cities_forecast_dict),
-                    err => {
-                      if (err) throw err;
-                      console.log('File written to...');
-                    }
-                );
-            } )
-        } 
-    }
-    
-}*/
+var cities_forecast_dict = {'Alabama': {'Montgomery':''}, 'Alaska': {'Juneau': ''}, 'Arizona': {'Phoenix': ''}, 'Arkansas': {'Little Rock': ''}, 'California': {'Sacramento': ''},    
+'Connecticut': {'Hartford': ''},'Colorado': {'Denver': ''}, 'Delaware': {'Dover': ''}, 'Florida': {'Tallahassee': ''}, 'Georgia': {'Atlanta': ''},'Hawaii': {'Honolulu': ''}, 'Idaho': {'Boise': ''},   
+'Illinois': {'Springfield': ''}, 'Indiana': {'Indianapolis': ''}, 'Iowa': {'Des Moines': ''}, 'Kansas': {'Topeka': ''},  'Kentucky': {'Frankfort': ''}, 'Louisiana': {'Baton Rouge': ''},  
+'Maine': {'Augusta': ''}, 'Maryland': {'Annapolis': ''}, 'Massachusetts': {'Boston': ''}, 'Michigan': {'Lansing': ''}, 'Minnesota': {'St. Paul': ''}, 'Mississippi': {'Jackson': ''}, 
+'Missouri': {'Jefferson City': ''}, 'Montana': {'Helena': ''}, 'Nebraska': {'Lincoln': ''}, 'Nevada': {'Carson City': ''},
+'New Hampshire': {'Concord': ''}, 'New Jersey': {'Trenton': ''}, 'New Mexico': {'Santa Fe': ''}, 'New York': {'Albany': ''}, 'North Carolina': {'Raleigh': ''}, 'North Dakota': {'Bismarck': ''},
+'Ohio': {'Columbus': ''}, 'Oklahoma': {'Oklahoma City': ''}, 'Oregon': {'Salem': ''}, 'Pennsylvania': {'Harrisburg': ''}, 'Rhode Island': {'Providence': ''}, 'South Carolina': {'Columbia': ''}, 
+'South Dakota': {'Pierre': ''},'Tennessee': {'Nashville': ''}, 'Texas': {'Austin': ''}, 'Utah': {'Salt Lake City': ''},'Vermont': {'Montpelier': ''}, 'Virginia': {'Richmond': ''},
+ 'Washington': {'Olympia': ''}, 'West Virginia': {'Charleston': ''}, 'Wisconsin': {'Madison': ''}, 'Wyoming': {'Cheyenne': ''}};
 
 
 
-
-intialize_location_id()
-function intialize_location_id(){
-    for(state of Object.keys(cities_forecast_dict)){
-        for(city of Object.keys(cities_forecast_dict[state])){
-            var country_id = ''
-            var state_local_name = ''
-            fetch('http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city)
-            .then(response => response.json())
-            .then(loc => { 
-                country_id = loc[0]['Country']['ID']
-                state_local_name = loc[0]['AdministrativeArea']['LocalizedName']
-                if (country_id == 'US' && state_local_name == state){
-                    cities_forecast_dict[state][city] = loc[0]['Key']
-                }
-                else{
-                    console.log('error! ' + loc[0]['Country']['ID'] + ' or ' + loc[0]['AdministrativeArea']['LocalizedName']+ ' is not in the dictionary')
-                }
-            })
-            .then(res =>{
-                intialize_forecast_data(state, city);
-            })
+async function request(city, state){
+    const res = await fetch('https://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city);
+    const loc = await res.json();
+    var country_id = loc[0]['Country']['ID']
+    var state_local_name = loc[0]['AdministrativeArea']['LocalizedName'];
+    var list_index = 0
+    while(list_index < loc.length){
+        if (country_id == 'US' && state_local_name == state){
+            console.log(state_local_name + ' ' + state + ' ' + city)
+            return loc[list_index]['Key']
         }
+        else{
+            list_index++
+            country_id = loc[list_index]['Country']['ID']
+            state_local_name = loc[list_index]['AdministrativeArea']['LocalizedName'];
+        }   
     }
 }
 
-function intialize_forecast_data(state, city){
-    fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + cities_forecast_dict[state][city] + '?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&details=true')
-    .then(res => res.json())
-    .then(forecast_data =>{
-       var loc_key = cities_forecast_dict[state][city]
-        cities_forecast_dict[state][loc_key] = initialize_weather_map(forecast_data)
-    })
-    .then(res =>{
-        fs.readFile(path.join(__dirname, "./new_format.txt"), "utf8", (err, file_text) => {
-            if (err) throw err;
-        
-            if(file_text.length == 0){
-                var curr_date_id = new Date()
-                curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
-                var wacc_dict = {};
-                wacc_dict[curr_date_id] = cities_forecast_dict; 
-                fs.appendFile(
-                    path.join(__dirname, './', 'new_format.txt'),
-                    JSON.stringify(wacc_dict),
-                    err => {
-                    if (err) throw err;
-                    console.log('File written');
-                    }
-                );
-            }
-            else{
-                file_text = file_text.substring(0, file_text.length - 1)
-                file_text = file_text + ','
-                var curr_date_id = new Date()
-                curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
-                var wacc_dict = {};
-                wacc_dict[curr_date_id] = cities_forecast_dict; 
-                var added_dictionary = (JSON.stringify(wacc_dict))
-                added_dictionary = added_dictionary.substring(1, added_dictionary.length);
-                file_text = file_text + added_dictionary;
-                fs.writeFile(
-                    path.join(__dirname, './', 'new_format.txt'),
-                    file_text,
-                    err => {
-                    if (err) throw err;
-                    console.log('File appended');
-                    }
-                );
-                
-            }
-        });
-    })
-    .then(print =>{
-        //after();
-    })
+async function load(cities_forecast_dic){
+  for(state of Object.keys(cities_forecast_dic)){
+       for(city of Object.keys(cities_forecast_dic[state])){
+        cities_forecast_dic[state][city] = await request(city,state);
+        await intialize_forecast_data(cities_forecast_dic, state, city)
+       }        
+   }
+}
+
+async function intialize_forecast_data(cities_forecast_dic, state, city){
+    const res = await fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + cities_forecast_dic[state][city] + '?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&details=true')
+    const weath = await res.json();
+    const loc_key = cities_forecast_dic[state][city]
+    cities_forecast_dic[state][loc_key] = initialize_weather_map(weath)
 }
 
 function initialize_weather_map(forecast_data){
@@ -147,33 +68,178 @@ function initialize_weather_map(forecast_data){
     return wacc_forecast
 }
 
-function after(){
-    console.log(cities_forecast_dict)
+function write_file(cities_forecast_dic){
+    fs.readFile(path.join(__dirname, "./test_data.txt"), "utf8", (err, file_text) => {
+        if (err) throw err;
+    
+        if(file_text.length == 0){
+            var curr_date_id = new Date()
+            curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
+            var wacc_dict = {};
+            wacc_dict[curr_date_id] = cities_forecast_dic; 
+            fs.appendFile(
+                path.join(__dirname, './', 'test_data.txt'),
+                JSON.stringify(wacc_dict),
+                err => {
+                if (err) throw err;
+                console.log('File written');
+                }
+            );
+        }
+        else{
+            file_text = file_text.substring(0, file_text.length - 1)
+            file_text = file_text + ','
+            var curr_date_id = new Date()
+            curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
+            var wacc_dict = {};
+            wacc_dict[curr_date_id] = cities_forecast_dic; 
+            var added_dictionary = (JSON.stringify(wacc_dict))
+            added_dictionary = added_dictionary.substring(1, added_dictionary.length);
+            file_text = file_text + added_dictionary;
+            fs.writeFile(
+                path.join(__dirname, './', 'test_data.txt'),
+                file_text,
+                err => {
+                if (err) throw err;
+                console.log('File appended');
+                }
+            );
+        }
+    });
 }
 
+async function run_retrieval(){
+ var cities_forecast_dic = {'Alabama': {'Montgomery':''}, 'Alaska': {'Juneau': ''}, 'Arizona': {'Phoenix': ''}, 'Arkansas': {'Little Rock': ''}, 'California': {'Sacramento': ''},    
+ 'Connecticut': {'Hartford': ''},'Colorado': {'Denver': ''}, 'Delaware': {'Dover': ''}, 'Florida': {'Tallahassee': ''}, 'Georgia': {'Atlanta': ''},'Hawaii': {'Honolulu': ''}, 'Idaho': {'Boise': ''},   
+ 'Illinois': {'Springfield': ''}, 'Indiana': {'Indianapolis': ''}, 'Iowa': {'Des Moines': ''}, 'Kansas': {'Topeka': ''},  'Kentucky': {'Frankfort': ''}, 'Louisiana': {'Baton Rouge': ''},  
+ 'Maine': {'Augusta': ''}, 'Maryland': {'Annapolis': ''}, 'Massachusetts': {'Boston': ''}, 'Michigan': {'Lansing': ''}, 'Minnesota': {'St. Paul': ''}, 'Mississippi': {'Jackson': ''}, 
+ 'Missouri': {'Jefferson City': ''}, 'Montana': {'Helena': ''}, 'Nebraska': {'Lincoln': ''}, 'Nevada': {'Carson City': ''},
+ 'New Hampshire': {'Concord': ''}, 'New Jersey': {'Trenton': ''}, 'New Mexico': {'Santa Fe': ''}, 'New York': {'Albany': ''}, 'North Carolina': {'Raleigh': ''}, 'North Dakota': {'Bismarck': ''},
+ 'Ohio': {'Columbus': ''}, 'Oklahoma': {'Oklahoma City': ''}, 'Oregon': {'Salem': ''}, 'Pennsylvania': {'Harrisburg': ''}, 'Rhode Island': {'Providence': ''}, 'South Carolina': {'Columbia': ''}, 
+ 'South Dakota': {'Pierre': ''},'Tennessee': {'Nashville': ''}, 'Texas': {'Austin': ''}, 'Utah': {'Salt Lake City': ''},'Vermont': {'Montpelier': ''}, 'Virginia': {'Richmond': ''},
+  'Washington': {'Olympia': ''}, 'West Virginia': {'Charleston': ''}, 'Wisconsin': {'Madison': ''}, 'Wyoming': {'Cheyenne': ''}};
+ await load(cities_forecast_dic);
+ await write_file(cities_forecast_dic);
+}
 
-/*fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/329551?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB')
-    .then(response => response.json())
-    .then(data => {
-        fs.writeFile(
-          path.join(__dirname, './', 'hello.txt'),
-          JSON.stringify(data),
-          err => {
-            if (err) throw err;
-            console.log('File written to...');
+run_retrieval();
 
-            fs.appendFile(
-              path.join(__dirname, './', 'hello.txt'),
-              ' I love Node.js',
-              err => {
-                if (err) throw err;
-                console.log('File written to...');
-              }
-            );
-          }
-        );
-        console.log(data)
-    }).catch(err => console.log('err'))*/
+// var promises = []
+
+// intialize_location_id()
+// .then(() =>{
+//     console.log(cities_forecast_dict)
+// })
+// function intialize_location_id(){
+//     for(state of Object.keys(cities_forecast_dict)){
+//         for(city of Object.keys(cities_forecast_dict[state])){
+            
+//             promises.push(fetch('http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city)
+//             .then(response => response.json())
+//             .then(loc => { 
+//                 var country_id = ''
+//                 var state_local_name = ''
+//                 country_id = loc[0]['Country']['ID']
+//                 state_local_name = loc[0]['AdministrativeArea']['LocalizedName']
+//                 if (country_id == 'US' && state_local_name == state){
+//                     cities_forecast_dict[state][city] = loc[0]['Key']
+//                 }
+//                 else{
+//                     console.log('error! ' + loc[0]['Country']['ID'] + ' or ' + loc[0]['AdministrativeArea']['LocalizedName']+ 'or'+ city+ ' is not in the dictionary')
+//                 }
+//             })
+//             // .then(res =>{
+//             //     //intialize_forecast_data(state, city);
+//             // })
+//             )
+//         }
+//     }
+//     return Promise.all(promises);
+// }
+
+// intialize_location_id()
+// function intialize_location_id(){
+//     for(state of Object.keys(cities_forecast_dict)){
+//         for(city of Object.keys(cities_forecast_dict[state])){
+//             var country_id = ''
+//             var state_local_name = ''
+//             fetch('http://dataservice.accuweather.com/locations/v1/cities/US/search?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&q=' + city)
+//             .then(response => response.json())
+//             .then(loc => { 
+//                 country_id = loc[0]['Country']['ID']
+//                 state_local_name = loc[0]['AdministrativeArea']['LocalizedName']
+//                 if (country_id == 'US' && state_local_name == state){
+//                     cities_forecast_dict[state][city] = loc[0]['Key']
+//                 }
+//                 else{
+//                     console.log('error! ' + loc[0]['Country']['ID'] + ' or ' + loc[0]['AdministrativeArea']['LocalizedName']+ 'or'+ city+ ' is not in the dictionary')
+//                 }
+//             })
+//             .then(res =>{
+//                 //intialize_forecast_data(state, city);
+//             })
+//         }
+//     }
+// }
+
+// function intialize_forecast_data(state, city){
+//     fetch('http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + cities_forecast_dict[state][city] + '?apikey=8iaBpn2Bl4GZtA9cgdXZT0dtpW30DGiB&details=true')
+//     .then(res => res.json())
+//     .then(forecast_data =>{
+//        var loc_key = cities_forecast_dict[state][city]
+//         cities_forecast_dict[state][loc_key] = initialize_weather_map(forecast_data)
+//     })
+//     .then(res =>{
+//         fs.readFile(path.join(__dirname, "./new_format.txt"), "utf8", (err, file_text) => {
+//             if (err) throw err;
+        
+//             if(file_text.length == 0){
+//                 var curr_date_id = new Date()
+//                 curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
+//                 var wacc_dict = {};
+//                 wacc_dict[curr_date_id] = cities_forecast_dict; 
+//                 fs.appendFile(
+//                     path.join(__dirname, './', 'new_format.txt'),
+//                     JSON.stringify(wacc_dict),
+//                     err => {
+//                     if (err) throw err;
+//                     console.log('File written');
+//                     }
+//                 );
+//             }
+//             else{
+//                 file_text = file_text.substring(0, file_text.length - 1)
+//                 file_text = file_text + ','
+//                 var curr_date_id = new Date()
+//                 curr_date_id = '' + curr_date_id.getFullYear() + '_' + (curr_date_id.getMonth() + 1) + '_' + curr_date_id.getDate() + '_' + curr_date_id.getHours()
+//                 var wacc_dict = {};
+//                 wacc_dict[curr_date_id] = cities_forecast_dict; 
+//                 var added_dictionary = (JSON.stringify(wacc_dict))
+//                 added_dictionary = added_dictionary.substring(1, added_dictionary.length);
+//                 file_text = file_text + added_dictionary;
+//                 fs.writeFile(
+//                     path.join(__dirname, './', 'new_format.txt'),
+//                     file_text,
+//                     err => {
+//                     if (err) throw err;
+//                     console.log('File appended');
+//                     }
+//                 );
+                
+//             }
+//         });
+//     })
+//     .then(print =>{
+//         //after();
+//     })
+// }
+
+
+
+// function after(){
+//     console.log(cities_forecast_dict)
+// }
+
 
 
   
--- test_code/homepage.js
+++ test_code/homepage.js
@@ -45,16 +45,16 @@ function parse_coords(coord_string){
 
 /*  draws the outline of a state */
 function state_hovered(state_img, state_area){
-    state_img.style.position = "absolute";
-    state_img.style.width = "1400px"
+    /*state_img.style.position = "absolute";
+    state_img.style.width = "1392px"
     state_img.style.height = "695px"
-    state_img.style.marginLeft = "4.4vw";
+    state_img.style.marginLeft = "4.7vw";
     state_img.style.marginTop = "2.9vw";
     state_img.style.zIndex = "20"
     state_img.style.display = "initial"
-    var hovd = false;
+    var hovd = false;*/
     
-    $(state_area).hover(function hovering(){       
+    /*$(state_area).hover(function hovering(){       
             hovd = true
             state_img.style.position = "absolute";
             state_img.style.width = "1400px"
@@ -64,25 +64,25 @@ function state_hovered(state_img, state_area){
             state_img.style.zIndex = "20";
             state_img.style.display = "initial";
         }, function non_hover(){
-        /*var refresh = window.setInterval(checkbound, 3);
+        var refresh = window.setInterval(checkbound, 3);
         function checkbound(){
             if(cnt % 2 == 0 && state_img.style.display == "initial"){
                 state_img.style.display = "none"
                 clearInterval(refresh);
             }
-        }  */ 
+        }  
             state_img.style.display = "none";
             $(state_area).off('mouseenter mouseleave');
             hovd=false;
         })
     
-    var hoving = window.setInterval(checkhov, 100);
+    var hoving = window.setInterval(checkhov, 20);
     function checkhov(){
         if(hovd == false){
             state_img.style.display = "none";
             window.clearInterval(hoving);
         }
-    }
+    }*/
 }
 
 /*function state_hovered(state_area_ele){
@@ -103,10 +103,6 @@ function state_hovered(state_img, state_area){
 }*/
 
 
-/*              *** icons map overlay ***               */
-var bg_map = document.querySelector('#background-map')
-var state_areas = bg_map.children;
-console.log(state_areas)
 
 /*                  *** accuweather API ***             */
 /*var cities = {'Alabama': ['Montgomery'], 'Alaska': ['Juneau'], 'Arizona': ['Phoenix'], 'Arkansas': ['Little Rock'], 'Connecticut': ['Hartford'], 'Delaware': ['Dover'], 'Florida': ['Tallahassee'], 'Georgia': ['Atlanta'], 'Indiana': ['Indianapolis'], 'Kentucky': ['Frankfort'], 'Maine': ['Augusta'], 'Maryland': ['Annapolis'], 'Massachusetts': ['Boston'], 'Mississippi': ['Jackson'], 'New Hampshire': ['Concord'], 'New Jersey': ['Trenton'], 'New York': ['Albany'], 'North Carolina': ['Raleigh'], 'Ohio': ['Columbus'], 'Pennsylvania': ['Harrisburg'], 'Rhode Island': ['Providence'], 'South Carolina': ['Pierre'], 'Tennessee': ['Nashville'], 'Vermont': ['Montpelier'], 'Virginia': ['Richmond'], 'West Virginia': ['Charleston']}
@@ -118,7 +114,50 @@ async function getdata(){
 }*/
 
 
+/*              *** icons map overlay ***               */
+var states_map = document.querySelector('#background-map')
+var state_icon = {}
+for (area of states_map.children){
+    state_icon[area.getAttribute('alt')] = mid_point_state(area.getAttribute('coords')); 
+}
+const LEFT_OFFSET = 73;
+const TOP_OFFSET = 913
+var icn = document.querySelector('#test-icon');
+var width = state_icon['Tenessee'][0]
+var height = state_icon['Tenessee'][1]
+width = width + LEFT_OFFSET
+height = height + TOP_OFFSET
+icn.style.position = "absolute"
+icn.style.left = toString(width) + 'px'
+icn.style.top = toString(height) + 'px'
+icn.style.width = '45px'
+icn.style.height = '30px'
 
+console.log(state_icon)
+function mid_point_state(coords){
+    var arr_coords = coords.split(',');
+    var i = 0;
+    for(str of arr_coords){
+        arr_coords[i] = parseInt(arr_coords[i])
+        i++;
+    }
+    var inc = 0;
+    var mid_point= [];
+    var total_x = 0;
+    var total_y = 0;
+    while(inc < arr_coords.length/2){
+        total_x = total_x + arr_coords[inc * 2];
+        inc++;
+    }
+    inc = 1;
+    while(inc < arr_coords.length/2 + 1){
+        total_y = total_y + arr_coords[(inc * 2) - 1];
+        inc++;
+    }
+    mid_point.push(total_x/(arr_coords.length/2));
+    mid_point.push(total_y/(arr_coords.length/2));
+    return mid_point;
+}
 
 /* used for seeing coordinates at location clicked */
 window.addEventListener('click', get_clicked_coords);
