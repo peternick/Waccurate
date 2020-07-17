528a8dec2163e8ab6fbae2dbf669fe1df272bca0
PeterA <50685884+peternick@users.noreply.github.com>
2020-07-17 08:53:53 -0400

    found and fixed bug for database manager
    Found and fixed a bug where the function to return the real feel statistic returned undefined.

--- data_retrieval/database_manager.js
+++ data_retrieval/database_manager.js
@@ -63,38 +63,41 @@ async function associate_data(){
     for(hist_file of Object.keys(file_match_up)){
         console.log('outer')
         try{
+            let forecast_morning = ''
+            let forecast_evening = ''
             if(parseInt(file_match_up[hist_file][1].split('_')[2]) + 1 == parseInt(hist_file.split('_')[2])){
-                let str_parsed_forecast = {}
-                let forecast_morning = file_match_up[hist_file][0]
-                let forecast_evening = file_match_up[hist_file][1]
-
-                let morning_json = await parse_file_text(forecast_morning, hist_file)
-                let evening_json = await parse_file_text(forecast_evening, hist_file)
-                let hist_json = await parse_file_text(hist_file, hist_file)
-                
-                console.log(get_real_feel_temp_stat_test(morning_json, evening_json, hist_json, 4))
-                console.log('end')
+                forecast_morning = file_match_up[hist_file][0]
+                forecast_evening = file_match_up[hist_file][1]         
             }
             else{
-                console.log('element whose date doesnt go in order')
+                console.log('element whose date is out of order')
+                forecast_morning = file_match_up[hist_file][1]
+                forecast_evening = file_match_up[hist_file][0]
             }
+            let morning_json = await parse_file_text(forecast_morning, hist_file, 'am')
+            let evening_json = await parse_file_text(forecast_evening, hist_file, '')
+            let hist_json = await parse_file_text(hist_file, hist_file, '')
+            
+            console.log(get_real_feel_temp_stat(morning_json, evening_json, hist_json, 4))
+            console.log('end')
+            
         }
         catch(err){
-            console.log(err)
+            console.log('from function -associate_data()- ' + err)
         }
     }
 }
 
-/*helper function that takes a text file that gets parsed into a JS object and retrieves data from a certain date of a forecast*/
+/*helper function that takes a text file which gets parsed into a JS object and retrieves data from a certain date of a forecast*/
 // text_data: a text file representing a customized json file consisting of either forecast or historical forecast data
 // history_file: the file containing historical forecasts of certain forecast text files
-async function parse_file_text(text_data, history_file){
+async function parse_file_text(text_data, history_file, period){
     let hour = ''
     let date = 0
     let file_date = history_file.split('_')
     let folder_path = ''
     if(text_data != hist_file){
-        if(text_data == file_match_up[hist_file][0]){
+        if(period == 'am'){
             hour = '8'
         }
         else{
@@ -123,75 +126,13 @@ async function parse_file_text(text_data, history_file){
 }
 
 
-//test_stats_setup()
-async function test_stats_setup(){
-    let hist_file = '/2020_7_10_9_24hr_hist.txt'
-    let morning_data = await new Promise((resolve, reject) =>{
-        fs.readFile(path.join(__dirname, forecasts_folder_path  + '/2020_7_8_20_12hr_forecast.txt'), "utf8", (err, file_text) => {
-            if(err) {
-                reject(err)
-                return;
-            }
-            let parsed_json_forecast_am = JSON.parse(file_text)
-            let file_date = hist_file.split('_')
-            file_date = file_date[0].substring(1,file_date[0].length) + '_' + file_date[1] + '_' + (parseInt(file_date[2]) - 1) + '_' + '8'
-            parsed_json_forecast_am = parsed_json_forecast_am[file_date]
-            resolve(parsed_json_forecast_am)
-        });
-    })
-    let evening_data = await new Promise((resolve, reject) =>{
-        fs.readFile(path.join(__dirname, forecasts_folder_path  +'/2020_7_9_20_12hr_forecast.txt'), "utf8", (err, file_text) => {
-            if(err) {
-                reject(err)
-                return;
-            }
-            let parsed_json_forecast_pm = JSON.parse(file_text)
-            let file_date = hist_file.split('_')
-            file_date = file_date[0].substring(1,file_date[0].length) + '_' + file_date[1] + '_' + (parseInt(file_date[2]) - 1) + '_' + '20'
-            parsed_json_forecast_pm = parsed_json_forecast_pm[file_date]
-            resolve(parsed_json_forecast_pm)
-        });
-    })
-    let hist_data = await new Promise((resolve, reject) =>{
-        fs.readFile(path.join(__dirname, historical_data_folder_path  +'/' + hist_file), "utf8", (err, file_text) => {
-            if(err) {
-                reject(err)
-                return;
-            }
-            let parsed_json_hist = JSON.parse(file_text)
-            let file_date = hist_file.split('_')
-            file_date = file_date[0].substring(1,file_date[0].length) + '_' + file_date[1] + '_' + file_date[2] + '_' + '9'
-            parsed_json_hist = parsed_json_hist[file_date]
-            resolve(parsed_json_hist)
-        });
-    })
-    await morning_data
-    await evening_data
-    await hist_data
-    //console.log(morning_data)
-    console.log(get_real_feel_temp_stat_test(morning_data, evening_data, hist_data, 3))
-
-}
-
 function get_real_feel_temp_stat(morning, evening, hist, precision){
     let real_feel_forecast = {}
     let real_feel_hist = {}
 
-    create_real_feel_dic(morning, real_feel_forecast)
-    create_real_feel_dic(evening, real_feel_forecast)
-    create_real_feel_dic(hist, real_feel_hist)
-    function create_real_feel_dic(json_data, real_feel_dic){
-        for(state of Object.keys(json_data)){
-            if(json_data != evening){
-                real_feel_dic[state] = {}
-            }
-            let loc_code = Object.keys(json_data[state])[0]
-            let hr_statistic_dic = json_data[state][loc_code]
-            for(hour of Object.keys(hr_statistic_dic)){
-                real_feel_dic[state][hour] = json_data[state][loc_code][hour]["RealFeelTemperature"]
-            }
-        }
-    }
+    create_real_feel_dic(morning, real_feel_forecast, true)
+    create_real_feel_dic(evening, real_feel_forecast, false)
+    create_real_feel_dic(hist, real_feel_hist, true)
     
     let precision_accuracy = []
     for(state of Object.keys(real_feel_hist)){
@@ -210,8 +151,6 @@ function get_real_feel_temp_stat(morning, evening, hist, precision){
             }
         }
     }
-
-
     let accurates = 0
     let total = 0
     let real_feel_percentage = 0
@@ -219,16 +158,22 @@ function get_real_feel_temp_stat(morning, evening, hist, precision){
         if(statistic == 'accurate'){
             accurates++
         }
-        else{
-            console.log('possible error')
-        }
         total++
     }
     real_feel_percentage = accurates/total
     return real_feel_percentage
 }
 
-
-function intitiate_stat_retrieval(morning, evening, hist){
-
+/*creates a dictionary of real feel temperature statistics retrieved from the custom made files based on Accuweather API data*/
+function create_real_feel_dic(json_data, real_feel_dic, bool_new_dic){
+    for(state of Object.keys(json_data)){
+        if(bool_new_dic){
+            real_feel_dic[state] = {}
+        }
+        let loc_code = Object.keys(json_data[state])[0]
+        let hr_statistic_dic = json_data[state][loc_code]
+        for(hour of Object.keys(hr_statistic_dic)){
+            real_feel_dic[state][hour] = json_data[state][loc_code][hour]["RealFeelTemperature"]
+        }
+    }
 }
